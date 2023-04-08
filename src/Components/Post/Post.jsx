8a2eb999c347ec16c1e1ae574a6abb25848448af
ricardo-719 <teacherrickyricardo@gmail.com>
2023-04-08 12:01:36 +0700

    Add Post component formatting

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -6,9 +6,14 @@ export const Post = (props) => {
     
   return (
     <section className="postItem">
-        <h2>{ post.title }</h2>
-        <p>Picture or Content HERE</p>
+        <span className='voteCount' >{post.ups}</span>
+        <div className='postSummary'>
+          <span className='postAuthor'>Posted by {post.author}</span>
+          <h3>{ post.title }</h3>
+          <p>Preview goes here</p>
+        </div>
     </section>
   )
 }
 
+//preview.images[0].source.url (But not all post contain images. A conditional must be put in place to avoid undefined issues)
