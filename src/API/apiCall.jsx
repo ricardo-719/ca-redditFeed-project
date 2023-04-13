6de7d403c33d8b5962c25eb8896d93bf12951828
ricardo-719 <teacherrickyricardo@gmail.com>
2023-04-13 08:18:40 +0700

    Clean code and refactor isError to be used with async thunk

--- src/API/apiCall.jsx
+++ src/API/apiCall.jsx
@@ -1,32 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-/* export const fetchPostData = async(subReddit) => {
-    try {
-      const response = await fetch(`https://www.reddit.com/r/${subReddit}.json`);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        
-        return jsonResponse.data.children.map((post) => post.data);
-      }
-      throw new Error('Request failed!');
-    } catch (error) {
-      console.log(error);
-    }
-} */
-
-/* const fetchPostData = async(endpoint) => {
-  try {
-    const response = await fetch(`https://www.reddit.com/r/${endpoint}.json`);
-    if (response.ok) {
-      const jsonResponse = await response.json();
-       setPosts(jsonResponse.data.children);
-      return jsonResponse.data.children
-    }
-  } catch (error) {
-    console.log(error);
-  }
-} */
-
 export const getPostData = createAsyncThunk('fetch/getPostData', async(subReddit) => {
     const response = await fetch(`https://www.reddit.com/r/${subReddit}.json`);
     const jsonResponse = await response.json();
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -8,66 +8,40 @@ import { getPostData } from '../../API/apiCall';
 
 export function Home() {
 
- /*  const [posts, setPosts] = useState(""); */
-  /* const [subReddit, setSubReddit] = useState('popular'); */
-/*   const [isLoading, setIsLoading] = useState(true); */
-  /* const [isError, setIsError] = useState(false); */
   const { subReddit, posts, isLoading, isError } = useSelector((state) => state.fetch)
   const dispatch = useDispatch();
-  /* const { posts, subReddit, isLoading, isError } = useSelector((state) => state.fetch) */
 
-// Fetch data from reddit
-/*   const fetchData = useCallback(async(endpoint) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(`https://www.reddit.com/r/${endpoint}.json`);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        setPosts(jsonResponse.data.children);
-      }
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-      setIsError(true);
-    }
-  } ,[]) */
 
   useEffect(() => {
-    /* fetchData(subReddit); */
     dispatch(getPostData(subReddit))
   }, [subReddit])
 
-  /* useEffect(() => {
-    if (posts) {
-      setIsLoading(false)
-    } else {
-      setIsLoading(true)
-    }
-  }, [posts]) */
-console.log(isLoading);
-console.log(posts)
-if (isLoading) {
-  return <h2>Loading...</h2>
-} 
-else if (isError) {
-  return (
-    <section>
-      <h2>Failed to load...</h2>
-      <button onClick={fetchData}>Try again</button>
-    </section>
-  )
-}
-else {
-  return (
-    <main>
-      <Header subReddit={subReddit} />
-      <Search />
-      <div className='posts'>
-        {
-          (posts != null) ? posts.map((post) => <Post key={post.data.id} post={post.data} />) : ''
-        }
-      </div> 
-    </main>
-  )
-}
+  const handleClick = () => {
+    dispatch(getPostData(subReddit))
+  }
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  } 
+  else if (isError) {
+    return (
+      <section>
+        <h2>Failed to load...</h2>
+        <button onClick={handleClick}>Try again</button>
+      </section>
+    )
+  }
+  else {
+    return (
+      <main>
+        <Header subReddit={subReddit} />
+        <Search />
+        <div className='posts'>
+          {
+            (posts != null) ? posts.map((post) => <Post key={post.data.id} post={post.data} />) : ''
+          }
+        </div> 
+      </main>
+    )
+  }
 }
